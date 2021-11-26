ddd143f85d25164d0e2cc816f4ac38dd067dec66
Daniil <sjprobad@gmail.com>
2021-11-26 17:11:18 +0300

    Practice#2

---
+++ JS/practice#2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+if (1) {
+    console.log('OK');
+} else {
+    console.log('Pizdezh');
+}
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('No');
+        break;
+    case 51:
+        console.log('Yes!');
+        break;
+    default:
+        console.log('not today!');
+        break;
+}
