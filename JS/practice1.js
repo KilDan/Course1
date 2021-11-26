659a9000a25ee1e79b35bb217c71fa85cd067e34
Daniil <sjprobad@gmail.com>
2021-11-26 18:15:36 +0300

    Practice#4

--- JS/practice1.js
+++ JS/practice1.js
@@ -1,19 +1,34 @@
-"use strict";
+// "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-let persolanMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    geners: {},
-    privat: false
-};
+// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// let persolanMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     geners: {},
+//     privat: false
+// };
 
 
-for (let index = 0; index < persolanMovieDB.count; index++) {
-    const a = prompt("Какой Фильм вы посмотрели?", ""),
-          b = prompt("Дайте оценку фильму", "");
-    persolanMovieDB.movies[a] = b;
-}
+// for (let index = 0; index < persolanMovieDB.count; index++) {
+//     let a, b;
+//     do {
+//         alert("Вводите данные корректно");
+//         a = prompt("Какой Фильм вы посмотрели?", "");
+//         b = prompt("Дайте оценку фильму", "");
+//     } 
+//     while (typeof(a) == 'object' && typeof(b) == 'object' && a.length > 10 );
+//     persolanMovieDB.movies[a] = b;
+// }
 
-console.log(persolanMovieDB);
+
+// if (persolanMovieDB.count < 10) {
+//     alert("Вы мало посмотрели");
+// } else if (persolanMovieDB.count < 30 && persolanMovieDB.count > 10 ) {
+//     alert("Вы - классический зритель");
+// } else if (persolanMovieDB.count > 30) {
+//     alert("Вы - киноман");
+// }
+
+// console.log(persolanMovieDB);
