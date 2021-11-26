d00dc9802c7762acf11676a69a65844b32282fbc
Daniil <sjprobad@gmail.com>
2021-11-26 17:21:54 +0300

    Practice#3

---
+++ JS/practice#3.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+
+
+for (let i = 1; i < 10; i++) {    
+    if (i == 6) {
+        continue;
+    }
+
+    console.log(i);
+}
