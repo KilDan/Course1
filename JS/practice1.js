e164a0a7b10c28c890eb0fd93d4e79d945aa9119
Daniil <sjprobad@gmail.com>
2021-11-26 21:29:50 +0300

    practice 6 functionPractice

--- JS/practice1.js
+++ JS/practice1.js
@@ -1,34 +1,65 @@
-// "use strict";
+"use strict";
 
-// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+let genres = [];
 
-// let persolanMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     geners: {},
-//     privat: false
-// };
+function writeYourGenres(){
+    for (let index = 0; index < 3; index++) {
+       persolanMovieDB.genres[index] = prompt(`Ваш любимый жанр ${index + 1}`);
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(persolanMovieDB);
+    }
+}
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
+
+let persolanMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: {},
+    privat: false
+};
 
 
-// for (let index = 0; index < persolanMovieDB.count; index++) {
-//     let a, b;
-//     do {
-//         alert("Вводите данные корректно");
-//         a = prompt("Какой Фильм вы посмотрели?", "");
-//         b = prompt("Дайте оценку фильму", "");
-//     } 
-//     while (typeof(a) == 'object' && typeof(b) == 'object' && a.length > 10 );
-//     persolanMovieDB.movies[a] = b;
-// }
 
 
-// if (persolanMovieDB.count < 10) {
-//     alert("Вы мало посмотрели");
-// } else if (persolanMovieDB.count < 30 && persolanMovieDB.count > 10 ) {
-//     alert("Вы - классический зритель");
-// } else if (persolanMovieDB.count > 30) {
-//     alert("Вы - киноман");
-// }
+function rememberMyFilms() {
+    for (let index = 0; index < persolanMovieDB.count; index++) {
+        let a, b;
+        do {
+            alert("Вводите данные корректно");
+            a = prompt("Какой Фильм вы посмотрели?", "");
+            b = prompt("Дайте оценку фильму", "");
+        }
+        while (a == null || b == null || a.length > 10);
+        persolanMovieDB.movies[a] = b;
+    }
+}
 
-// console.log(persolanMovieDB);
+rememberMyFilms();
+
+function detectPersonaLevel() {
+    if (persolanMovieDB.count < 10) {
+        alert("Вы мало посмотрели");
+    } else if (persolanMovieDB.count < 30 && persolanMovieDB.count > 10) {
+        alert("Вы - классический зритель");
+    } else if (persolanMovieDB.count > 30) {
+        alert("Вы - киноман");
+    }
+}
+detectPersonaLevel();
+
+console.log(persolanMovieDB);
