4ba337020b03f35feb53fada8a30ab228c8dc5e3
Daniil <sjprobad@gmail.com>
2021-11-26 16:54:29 +0300

    Practice #1

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let persolanMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: {},
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?','');
+persolanMovieDB.movies[a] = b;
+persolanMovieDB.movies[c] = d;
+
+console.log(persolanMovieDB);
